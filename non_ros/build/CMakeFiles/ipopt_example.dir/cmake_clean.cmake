b3f84a0fe0da7ecc52d8202ccac7f16caa98f074
nach96 <n.cavero.juste@gmail.com>
2020-10-25 11:26:29 +0100

    Folder to program OC problem outside ros

---
+++ non_ros/build/CMakeFiles/ipopt_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ipopt_example.dir/cpp_example.cpp.o"
+  "ipopt_example.pdb"
+  "ipopt_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ipopt_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ non_ros/build/CMakeFiles/myNLP.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libmyNLP.a"
+)
